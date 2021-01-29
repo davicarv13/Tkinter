1deb798ef57ea8eebc600d8e23ea426e5c2f6515
davicarv13 <davicarv13@gmail.com>
2021-01-29 08:05:46 -0300

    Optimized openFile function

--- files.py
+++ files.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 root = Tk()
 
 def openFile():
-    filepath = filedialog.askopenfilename()
+    filepath = filedialog.askopenfilename(initialdir="/home/davi/", title="Select a file:", filetypes=(('png files', "*.png"),("all files", "*.*")))
     print(filepath)
 
 button = Button(root, text="Click", command=openFile)
