5d45a2849998755574bf82da55d90df203dfa564
davicarv13 <davicarv13@gmail.com>
2021-02-02 16:49:12 -0300

    Add anchor.py

---
+++ anchor.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+#N = Norte, S=Sul, E=Lest, W=Oeste
+#NE = Nordeste, SE = Sudeste, SW=Sudoeste, NW=Noroeste
+Label(root, text="Nome", background="#fff", foreground="#009", anchor=E).place(x=10, y=10, width=100, height=100)
+
+
+root.mainloop()
